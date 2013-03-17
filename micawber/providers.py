874b60e1c8c481f4bd1d07c4421289e921adaa61
Diederik van der Boor <vdboor@edoburu.nl>
2013-03-17 10:41:42 +0100

    Add new providers discovered via Embedly/WordPress and some good guesses..
    This adds:
    
    * flic.kr
    * gist.github.com
    * mobypicture / moby.to
    * polldaddy,
    * scribd.com
    * slidesha.re
    * speakerdesk
    * yfrog

--- micawber/providers.py
+++ micawber/providers.py
@@ -129,7 +129,11 @@ def bootstrap_basic(cache=None):
 
     # f
     pr.register('http://\S*?flickr.com/\S+', Provider('http://www.flickr.com/services/oembed/'))
+    pr.register('http://flic\.kr/\S*', Provider('http://www.flickr.com/services/oembed/'))
     pr.register('https?://(www\.)?funnyordie\.com/videos/\S+', Provider('http://www.funnyordie.com/oembed'))
+
+    # g
+    pr.register(r'https?://gist.github.com/\S*', Provider('https://github.com/api/oembed'))
 
     # h
     pr.register('http://www.hulu.com/watch/\S+', Provider('http://www.hulu.com/api/oembed.json'))
@@ -142,10 +146,15 @@ def bootstrap_basic(cache=None):
     # j
     pr.register('http://www.jest.com/(video|embed)/\S+', Provider('http://www.jest.com/oembed.json'))
 
+    # m
+    pr.register('http://www.mobypicture.com/user/\S*?/view/\S*', Provider('http://api.mobypicture.com/oEmbed'))
+    pr.register('http://moby.to/\S*', Provider('http://api.mobypicture.com/oEmbed'))
+
     # p
     pr.register('http://i\S*.photobucket.com/albums/\S+', Provider('http://photobucket.com/oembed'))
     pr.register('http://gi\S*.photobucket.com/groups/\S+', Provider('http://photobucket.com/oembed'))
     pr.register('http://www.polleverywhere.com/(polls|multiple_choice_polls|free_text_polls)/\S+', Provider('http://www.polleverywhere.com/services/oembed/'))
+    pr.register('https?://(.+\.)?polldaddy\.com/\S*', Provider('http://polldaddy.com/oembed/'))
 
     # q
     pr.register('http://qik.com/video/\S+', Provider('http://qik.com/api/oembed.json'))
@@ -155,8 +164,11 @@ def bootstrap_basic(cache=None):
 
     # s
     pr.register('http://www.slideshare.net/[^\/]+/\S+', Provider('http://www.slideshare.net/api/oembed/2'))
+    pr.register('http://slidesha\.re/\S*', Provider('http://www.slideshare.net/api/oembed/2'))
     pr.register('http://\S*.smugmug.com/\S*', Provider('http://api.smugmug.com/services/oembed/'))
     pr.register('https://\S*?soundcloud.com/\S+', Provider('http://soundcloud.com/oembed'))
+    pr.register('https?://speakerdeck\.com/\S*', Provider('https://speakerdeck.com/oembed.json')),
+    pr.register('https?://(www\.)?scribd\.com/\S*', Provider('http://www.scribd.com/services/oembed'))
 
     # t
     pr.register('https?://(www\.)?twitter.com/\S+/status(es)?/\S+', Provider('http://api.twitter.com/1/statuses/oembed.json'))
@@ -168,6 +180,7 @@ def bootstrap_basic(cache=None):
 
     # y
     pr.register('https?://(\S*.)?youtu(\.be/|be\.com/watch)\S+', Provider('http://www.youtube.com/oembed'))
+    pr.register('http://(\S*\.)?yfrog\.com/\S*', Provider('http://www.yfrog.com/api/oembed'))
 
     # w
     pr.register('http://\S+.wordpress.com/\S+', Provider('http://public-api.wordpress.com/oembed/'))
