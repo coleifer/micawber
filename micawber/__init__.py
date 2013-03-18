5673bf2b95c9fd04caf9cda4792890a8ff8ca751
Diederik van der Boor <vdboor@edoburu.nl>
2013-03-18 11:46:30 +0100

    Add missing export of `bootstrap_noembed()` to `micawber/__init__.py`

--- micawber/__init__.py
+++ micawber/__init__.py
@@ -1,4 +1,4 @@
 from micawber.cache import Cache, PickleCache
 from micawber.exceptions import ProviderException
 from micawber.parsers import extract, extract_html, parse_text, parse_text_full, parse_html
-from micawber.providers import Provider, ProviderRegistry, bootstrap_basic, bootstrap_embedly
+from micawber.providers import Provider, ProviderRegistry, bootstrap_basic, bootstrap_embedly, bootstrap_noembed
