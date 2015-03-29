40a3296f72cec3586d61e3939723422509eff8ff
Charles Leifer <coleifer@gmail.com>
2015-03-29 13:08:40 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='micawber',
-    version="0.3.1",
+    version='0.3.2',
     description='a small library for extracting rich content from urls',
     long_description=readme,
     author='Charles Leifer',
