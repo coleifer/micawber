76ac5ed3d619482c217e96d8be66bec8d1d12205
Richard Cornish <richardcornish@gmail.com>
2015-08-27 22:55:04 -0500

    Added ReverbNation provider

--- micawber/providers.py
+++ micawber/providers.py
@@ -251,6 +251,7 @@ def bootstrap_basic(cache=None, registry=None):
     pr.register('https://rapidengage.com/s/\S+', Provider('https://rapidengage.com/api/oembed'))
     pr.register('http://(\S+\.)?rdio.com/(artist|people)/\S+', Provider('http://www.rdio.com/api/oembed/'))
     pr.register('http://rwire.com/\S+', Provider('http://publisher.releasewire.com/oembed/'))
+    pr.register('https://www.reverbnation.com/\S+', Provider('https://www.reverbnation.com/oembed'))
     pr.register('http://\S*.revision3.com/\S+', Provider('http://revision3.com/api/oembed/'))
     pr.register('http://roomshare.jp/(en/)?post/\S+', Provider('http://roomshare.jp/en/oembed.json'))
 
