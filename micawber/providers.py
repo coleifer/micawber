45d4e19a26ef51f4d0fd9a2f2e4557b1140fdd84
Richard Cornish <richardcornish@gmail.com>
2015-08-27 22:10:20 -0500

    Added Meetup provider

--- micawber/providers.py
+++ micawber/providers.py
@@ -209,6 +209,7 @@ def bootstrap_basic(cache=None, registry=None):
     pr.register('http://www.kickstarter.com/projects/\S+', Provider('http://www.kickstarter.com/services/oembed'))
 
     # m
+    pr.register('http://meetu(p\.com|\.ps)/\S+', Provider('https://api.meetup.com/oembed'))
     pr.register('http://www.mixcloud.com/[^\/]+/\S+', Provider('http://www.mixcloud.com/oembed/'))
     pr.register('http://www.mobypicture.com/user/\S+/view/\S+', Provider('http://api.mobypicture.com/oEmbed'))
     pr.register('http://moby.to/\S+', Provider('http://api.mobypicture.com/oEmbed'))
