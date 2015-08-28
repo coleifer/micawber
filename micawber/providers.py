ce688e43010121bba7998e3c02fabfd66dc416e3
Richard Cornish <richardcornish@gmail.com>
2015-08-27 21:36:38 -0500

    Added Roomshare provider

--- micawber/providers.py
+++ micawber/providers.py
@@ -216,6 +216,7 @@ def bootstrap_basic(cache=None, registry=None):
     # r
     pr.register('http://(\S+\.)?rdio.com/(artist|people)/\S+', Provider('http://www.rdio.com/api/oembed/'))
     pr.register('http://\S*.revision3.com/\S+', Provider('http://revision3.com/api/oembed/'))
+    pr.register('http://roomshare.jp/(en/)?post/\S+', Provider('http://roomshare.jp/en/oembed.json'))
 
     # s
     pr.register('http://videos.sapo.pt/\S+', Provider('http://videos.sapo.pt/oembed'))
