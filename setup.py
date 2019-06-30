96250d2a89238f0dfe969bc44412bfc89f276448
Charles Leifer <coleifer@gmail.com>
2019-06-30 11:19:47 -0500

    0.5.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='micawber',
-    version='0.4.1',
+    version='0.5.0',
     description='a small library for extracting rich content from urls',
     long_description=readme,
     author='Charles Leifer',
